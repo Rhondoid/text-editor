8bef7f688639b483c68852c9a2551cb86a3bb062
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-02-05 18:49:12 -0700

    setup workbox plugins, css loaders and babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,31 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+     new HtmlWebpackPlugin({
+      template: './index.html',
+      title:'Webpack Plugin',
+     }),
+     new WorkboxPlugin.GenerateSW(),
+     new WebpackPwaManifest(),
+     new InjectManifest()
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use:{
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
